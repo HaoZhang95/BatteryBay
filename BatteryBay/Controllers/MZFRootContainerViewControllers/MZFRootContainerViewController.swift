02fed54a7d46467f00d70cea9e5f962bfee97d46
NGUYEN BA LONG <Long.Nguyen@metropolia.fi>
2018-04-19 13:58:32 +0300

    onboarding and login vc

---
+++ BatteryBay/Controllers/MZFRootContainerViewControllers/MZFRootContainerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MZFRootContainerViewController.swift
+//  BatteryBay
+//
+//  Created by Nguyen Ba Long on 19/04/2018.
+//  Copyright © 2018 iosdev. All rights reserved.
+//
+
+import Foundation
---
+++ BatteryBay/Models/OnBoardingPage.swift
@@ -0,0 +1,14 @@
+//
+//  OnBoardingPage.swift
+//  BatteryBay
+//
+//  Created by Nguyen Ba Long on 19/04/2018.
+//  Copyright © 2018 iosdev. All rights reserved.
+//
+
+import Foundation
+struct OnboardingPage {
+    var title: String
+    var subtitle: String
+    var animationName: String
+}
